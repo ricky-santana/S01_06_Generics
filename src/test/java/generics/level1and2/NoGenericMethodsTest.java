bc77652f9eaf8f71666c145dd62a875af5cdaf4e
Richard <153839902+ricky-santana@users.noreply.github.com>
2026-07-15 18:29:48 +0200

    L1 Ex1: NoGenericMethodsTest assert that the class store and return ints regardless the order

--- src/test/java/generics/level1and2/NoGenericMethodsTest.java
+++ src/test/java/generics/level1and2/NoGenericMethodsTest.java
@@ -1,4 +1,26 @@
 package generics.level1and2;
 
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class NoGenericMethodsTest {
+
+    @Test
+    void shouldStoreAndShowElementRegardlessOrder(){
+        int numA = 10;
+        int numB = 55;
+        int numC = 99;
+
+        NoGenericMethods order1 = new NoGenericMethods(numA, numB, numC);
+        NoGenericMethods order2 = new NoGenericMethods(numB, numC, numA);
+
+        assertThat(order1.getElement1()).isEqualTo(numA);
+        assertThat(order1.getElement2()).isEqualTo(numB);
+        assertThat(order1.getElement3()).isEqualTo(numC);
+
+        assertThat(order2.getElement1()).isEqualTo(numB);
+        assertThat(order2.getElement2()).isEqualTo(numC);
+        assertThat(order2.getElement3()).isEqualTo(numA);
+
+    }
 }
